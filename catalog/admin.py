0655b9af6bc06b91323f50ed54e0053cf62aa1ab
diivanovsky <diivanovsky@gmail.com>
2023-10-03 22:55:14 +0300

    add migrations and one view of catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from catalog.models import Category, Discount, Product, Promocode, Seller
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'articul', 'category', 'seller')
+    search_fields = ('name', 'articul', 'seller__name', 'seller__country', 'category__name')
+    list_select_related = ('category', 'seller')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name',)
+
+
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')
+    search_fields = ('name', 'country')
+
+
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'percent', 'date_start', 'date_end')
+    search_fields = ('name', 'percent')
+
+
+class PromocodeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'percent', 'date_start', 'date_end', 'is_cumulative')
+    search_fields = ('name',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Discount, DiscountAdmin)
+admin.site.register(Promocode, PromocodeAdmin)
+admin.site.register(Seller, SellerAdmin)
+admin.site.register(Product, ProductAdmin)
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from catalog.models import Category, Product
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from catalog.serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoriesListView(ListAPIView):
+    queryset = Category.objects.all()
+    permission_classes = (AllowAny, )
+    serializer_class = CategorySerializer
