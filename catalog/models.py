859d54942b9c0a94a581c77fb6a1026aa383e5cd
diivanovsky <diivanovsky@gmail.com>
2023-10-04 19:10:39 +0300

    add all guest functionalities and basket

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import CustomUser
 
 
 class Category(models.Model):
@@ -54,3 +55,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField(null=True, blank=True)
